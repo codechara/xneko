b6c4b83d10ac6823435d10e0eaac42ba34c8c505
Chara <magacademy30@gmail.com>
2025-07-03 12:43:39 +0300

    r7ei4wsrdr64i6e

--- main.py
+++ main.py
@@ -1,56 +1,20 @@
-import os, pygame, moderngl, numpy as np
+import os, pygame, moderngl, numpy as np, build
 
 pygame.init()
 
 def main():
     path = os.path.split(os.path.abspath(__file__))[0]
 
-    pygame.display.set_mode((640, 480), pygame.OPENGL)
-    pygame.display.set_caption("test")
+    pygame.display.set_mode((800, 600), pygame.OPENGL | pygame.RESIZABLE)
+    pygame.display.set_caption(f"XNeko {build.VERSION}")
     pygame.display.set_icon(pygame.image.load(path+"/assets/icon.png").convert_alpha())
     ctx = moderngl.create_context()
-    for i in ctx.info:
-        print("%s : %s" % (i, ctx.info[i]))
-
-    prog = ctx.program(
-        vertex_shader="""
-            in vec2 in_vert;
-            in vec3 in_color;
-            
-            out vec3 v_color;
-            
-            void main() {
-                v_color = in_color;
-                gl_Position = vec4(in_vert, 0.0, 1.0);
-            }
-        """,
-        fragment_shader="""
-            in vec3 v_color;
-            
-            out vec3 f_color;
-            
-            void main() {
-                f_color = v_color;
-            }
-        """
-    )
-
-    verticles = np.array([
-        -0.5, -0.5,   1.0, 0.0, 0.0,
-         0.5, -0.5,   0.0, 1.0, 0.0,
-         0.0,  0.5,   0.0, 0.0, 1.0
-    ], dtype="f4")
-
-    vbo = ctx.buffer(verticles)
-    vao = ctx.simple_vertex_array(prog, vbo, "in_vert", "in_color")
 
     while True:
-        ctx.clear()
-        vao.render(moderngl.TRIANGLES)
+        ctx.clear(1.0, 0.0, 0.0)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 exit()
 
         pygame.display.flip()
